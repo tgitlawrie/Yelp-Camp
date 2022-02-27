d740c1a9053ed5e409ac1ebe97c0b901d9705925
tony <tilawrie@gmail.com>
2022-02-27 07:38:49 +0000

    updating map start

--- public/javascripts/clustermap.js
+++ public/javascripts/clustermap.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.5917, 40.6699],
+    center: [174.40112170509306,-41.31384621676557],
     zoom: 3
 });
 
